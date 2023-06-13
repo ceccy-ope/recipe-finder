342c2dd08c22fd4d744b370ea155652f307aaed4
ceccy-ope <muyidececilia@gmail.com>
2023-06-13 23:08:03 +0100

    added files

--- script.js
+++ script.js
@@ -66,6 +66,10 @@ function func() {
           }
           toggleModal()
 
+          function hideModal() {
+            modal.classList.remove("show-modal");
+          }
+
           function showModal() {
           
             let menuId = mealIdList[j];
@@ -78,6 +82,35 @@ function func() {
               })
               .then((data)=>{
                 console.log(data)
+                recipeDescription.innerHTML = `<h6 style = "text-align:center;">${mealNameList[j]}</h6>
+                                              <img style = "width:100px; height: 100px; border-radius:50%; display:block; margin:auto" src=${data.meals[0].strMealThumb}>
+                                              <h6>Ingredients</h6>
+                                              <p style="font-size:10px;font-weight:100;">
+                                              ${data.meals[0].strIngredient1}(${data.meals[0].strMeasure1}),
+                                              ${data.meals[0].strIngredient2}(${data.meals[0].strMeasure2}),
+                                              ${data.meals[0].strIngredient3}(${data.meals[0].strMeasure3}),
+                                              ${data.meals[0].strIngredient4}(${data.meals[0].strMeasure4}),
+                                              ${data.meals[0].strIngredient5}(${data.meals[0].strMeasure5}),
+                                              ${data.meals[0].strIngredient6}(${data.meals[0].strMeasure6}),
+                                              ${data.meals[0].strIngredient7}(${data.meals[0].strMeasure7}),
+                                              ${data.meals[0].strIngredient8}(${data.meals[0].strMeasure8}),
+                                              ${data.meals[0].strIngredient9}(${data.meals[0].strMeasure9}),
+                                              ${data.meals[0].strIngredient10}(${data.meals[0].strMeasure10}),
+                                              ${data.meals[0].strIngredient11}(${data.meals[0].strMeasure11}),
+                                              ${data.meals[0].strIngredient12}(${data.meals[0].strMeasure12}),
+                                              ${data.meals[0].strIngredient13}(${data.meals[0].strMeasure13}),
+                                              ${data.meals[0].strIngredient14}(${data.meals[0].strMeasure14}),
+                                              ${data.meals[0].strIngredient15}(${data.meals[0].strMeasure15}),
+                                              ${data.meals[0].strIngredient16}(${data.meals[0].strMeasure16}),
+                                              ${data.meals[0].strIngredient17}(${data.meals[0].strMeasure17}),
+                                              ${data.meals[0].strIngredient18}(${data.meals[0].strMeasure18}),
+                                              ${data.meals[0].strIngredient19}(${data.meals[0].strMeasure19}),
+                                              ${data.meals[0].strIngredient20}(${data.meals[0].strMeasure20}),
+                                              </p>
+                                              <h6>How To Prepare</h6>
+                                              <p style="font-size:10px;font-weight:100;">${data.meals[0].strInstructions}</p>
+                                              <a style = "font-size:10px;" href=${data.meals[0].strYoutube}>Youtube Video</a>
+                                              `;
 
               })
               .catch((error)=>{
@@ -85,14 +118,14 @@ function func() {
 
               })
             
-            recipeDescription.textContent = 'hello'
+            
             function windowOnClick(event) {
               if (event.target === modal) {
-                toggleModal();
+               hideModal();
               }
             }
 
-            closeButton.addEventListener("click", toggleModal);
+            closeButton.addEventListener("click", hideModal);
             window.addEventListener("click", windowOnClick);
           }
           showModal();
